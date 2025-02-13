346082f2943d5aba4a9c59a0160518ef6647adf2
Alejandro <aleiestudio@gmail.com>
2025-02-13 17:41:48 +0000

    añadida condicion especial de 1 solo filosofo...

--- philo/src/main.c
+++ philo/src/main.c
@@ -8,6 +8,16 @@ int main(int argc, char **argv)
 
     if (parse_args(argc, argv, &config))
         return (1);
+    if (config.num_filosofos == 1)
+    {
+        long start_time = get_time();
+
+        printf("[%ld] Philosopher 1 is thinking\n", get_time() - start_time);
+        printf("[%ld] Philosopher 1 has taken a fork\n", get_time() - start_time);
+        usleep(config.time_to_die * 1000);
+        printf("[%ld] Philosopher 1 died\n", get_time() - start_time);
+        return (0);
+    }
     init_simulation(&config, &philos, &forks);
     cleanup(&config, philos, forks);
     return (0);
